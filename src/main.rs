cb187a10de5068e146636bfc81da1e7d1b6e057a
Drew Zemke <dnzemke@ucdavis.edu>
2023-10-31 13:10:51 -0700

    feat: parse and pretty-print api response

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,58 @@
 use anyhow::{anyhow, Result};
-use std::env;
+use serde::Deserialize;
+use std::{env, fmt::Display};
+
+#[derive(Debug, Deserialize)]
+struct BreakoutRoomInfo {
+    name: String,
+    status: BreakoutRoomStatus,
+}
+
+impl Display for BreakoutRoomInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = format!("{}: {}", self.name, self.status);
+        s.fmt(f)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+enum BreakoutRoomStatus {
+    #[serde(rename = "open")]
+    Open,
+
+    #[serde(rename = "closed")]
+    Closed,
+}
+
+impl Display for BreakoutRoomStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            BreakoutRoomStatus::Open => "open",
+            BreakoutRoomStatus::Closed => "closed",
+        };
+        s.fmt(f)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct BoratResponse(Vec<BreakoutRoomInfo>);
+
+impl Display for BoratResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s: String = self.0.iter().map(|s| format!("  {s}\n")).collect();
+
+        s.fmt(f)
+    }
+}
 
 fn main() -> Result<()> {
     let url =
         env::var("BORAT_URL").map_err(|_| anyhow!("Missing environment variable `BORAT_URL`."))?;
 
-    let response = reqwest::blocking::get(url)?.text()?;
-    println!("response: {}", response);
+    let response = reqwest::blocking::get(url)?.json::<BoratResponse>()?;
+
+    println!("Breakout Room Statuses:");
+    println!("{}", response);
 
     Ok(())
 }
