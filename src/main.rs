c84b21e97b9e60a898d758f67964772bb189e01e
Drew Zemke <dnzemke@ucdavis.edu>
2023-10-31 11:04:01 -0700

    feat: get url and print response (gotta start small)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let url = env::var("BORAT_URL")?;
+
+    let response = reqwest::blocking::get(url)?.text()?;
+    println!("response: {}", response);
+
+    Ok(())
 }
